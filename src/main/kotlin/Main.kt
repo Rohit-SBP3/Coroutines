8df3fa27393bb0a3e7e45d358c57e6ed2409e3c8
Rohit Singh <rohitrajput95547@gmail.com>
2025-04-15 10:51:41 +0530

    Basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
+
+/***   Basic Concepts :-
+ * Program -> set of Instructions -> process -> threads(thread of execution)
+ *
+ * process :- (processId, state, memory, handle networking, fileSystem etc, threads).
+ *
+ * each program contain at least one thread.
+ *
+ * sequential execution :- process(instruction1 ------ instructionN)
+ *
+ * so can we just re-use the thread when it is waiting for some responses or IO operations?
+ *
+ * CO -> COOPERATING
+ * ROUTINES -> FUNCTIONS
+ *
+ * */
+
 fun main(args: Array<String>) {
     println("Hello Coroutines!")
 }
+
