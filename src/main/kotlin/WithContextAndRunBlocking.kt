90221d5a296bf6ee129adea0599d42b9461cf4c9
Rohit Singh <rohitrajput95547@gmail.com>
2025-04-22 16:29:28 +0530

    withContext and runBlocking

---
+++ src/main/kotlin/WithContextAndRunBlocking.kt
@@ -0,0 +1,34 @@
+import kotlinx.coroutines.*
+
+fun main() =  runBlocking  {
+
+    /*CoroutineScope(Dispatchers.IO).launch {
+        launch {  executeTask() }
+        launch { executeTask1() }
+    }
+    Thread.sleep(3500)*/
+
+    launch { executeTask() }
+    launch { executeTask1() }
+    println("All done!!!!")
+
+}
+
+
+suspend fun executeTask(){
+    println("Before withContext launch")
+    withContext(Dispatchers.IO) {
+        delay(3000)
+        println("Inside")
+    }
+    println("After withContext launch")
+}
+
+suspend fun executeTask1(){
+    println("Before Global scope launch")
+    GlobalScope.launch {
+        delay(2000)
+        println("Inside")
+    }.join()
+    println("After Global scope launch")
+}
