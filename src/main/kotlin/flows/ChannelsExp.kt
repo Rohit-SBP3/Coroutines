69a0617520832283db2d5acf218de57964fa25d6
Rohit Singh <rohitrajput95547@gmail.com>
2025-04-24 21:25:03 +0530

    Kotlin Flows

---
+++ src/main/kotlin/flows/ChannelsExp.kt
@@ -0,0 +1,28 @@
+package flows
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking{
+    val channel = Channel<Int>()
+    producer(channel)
+    consumer(channel)
+    Thread.sleep(4000)
+}
+
+fun producer(channel: Channel<Int>){
+    CoroutineScope(Dispatchers.IO).launch {
+        channel.send(1)
+        channel.send(2)
+    }
+}
+
+fun consumer(channel: Channel<Int>){
+    CoroutineScope(Dispatchers.IO).launch {
+        println(channel.receive())
+        println(channel.receive())
+    }
+}
